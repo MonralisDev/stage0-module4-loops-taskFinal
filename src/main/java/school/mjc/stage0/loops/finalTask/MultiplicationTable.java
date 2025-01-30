8c01d3f0b85dbd8d22cd99824bd3a7ce3ae2deb5
M <M>
2025-01-30 16:19:35 +0200

    +

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+        if (numberTableToPrint < 0 ){
+            return;
+        }
+        for(int i = 1; i <= 10; i++){
+            int multiplication = i * numberTableToPrint;
+            System.out.println(numberTableToPrint +  " * " + i + " " + "= " + multiplication);
+        }
+    }
+
+    public static void main(String[] args) {
+        MultiplicationTable multiplicationTable = new MultiplicationTable();
+        multiplicationTable.printTable(6);
     }
 }
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,21 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            // Друкуємо пробіли перед числами
+            for (int j = 0; j < cathetusLength - i; j++) {
+                System.out.print("  ");
+            }
+            // Друкуємо першу половину чисел
+            for (int j = i; j > 1; j--) {
+                System.out.print(j + " ");
+            }
+            // Друкуємо другу половину чисел
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println(); // Перехід на новий рядок
+        }
     }
 
     public static void main(String[] args) {
